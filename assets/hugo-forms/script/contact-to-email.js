75aed1fec4dd689aa12abb7dfe2f49a6dc86250a
Rye <git@itsrye.dev>
2025-12-28 14:44:13 +0100

    refactor: improve form submission handling and enhance attachment processing

--- assets/hugo-forms/script/contact-to-email.js
+++ assets/hugo-forms/script/contact-to-email.js
@@ -7,7 +7,7 @@ var formElement;
 function initializeContactForm(formId, targetEmail, emailIsObfuscated) {
     formElement = document.getElementById(formId);
 
-    formElement.addEventListener('submit', function(event) {
+    formElement.addEventListener('submit', function (event) {
         event.preventDefault();
         sendFormData(targetEmail);
     });
@@ -18,14 +18,39 @@ function sendFormData(targetEmail) {
     var emailBody = '';
     const subject = formData.get('subject') || 'Contact Form Submission';
     const attachment = formData.get('attachment');
+    const output = document.createElement("div");
 
-    formData.forEach(function(value, key) {
+    formData.forEach(function (value, key) {
         emailBody += "<strong>" + key + "</strong>: " + value + "\n";
     });
 
     if (attachment && attachment.size > 0) {
         emailBody += "Attachment included: " + attachment.name + "\n\n";
     }
+
+    const reader = new FileReader();
+    reader.onload = function () {
+        const base64Data = reader.result;
+        if (attachment.type.startsWith("image/")) {
+            const img = document.createElement("img");
+            img.src = base64Data;
+            img.style.maxWidth = "300px";
+            img.alt = file.name;
+            output.appendChild(img);
+        } else {
+            const link = document.createElement("a");
+            link.href = base64Data;
+            link.download = file.name;
+            link.textContent = `Download ${file.name}`;
+            link.style.display = "inline-block";
+            link.style.marginTop = "10px";
+
+            output.appendChild(link);
+        }
+    };
+    reader.readAsDataURL(attachment);
+
+    emailBody += "\n\n" + output.innerHTML;
 
     var mailtoLink = 'mailto:' + targetEmail + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(emailBody);
     window.location.href = mailtoLink;
